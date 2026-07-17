5f3dd0e72391f824d5bb901a9e3137fb0a5fc1d5
Adam Benoit <adam.benoit@openchip.com>
2026-07-17 13:09:00 +0200

    Minimal conversion up to dct
    This includes:
    Parse cmd - a minimal function to parse argument, only support option -f for now and returns a collections of lines from the open file
    help - shows the usage for the binary
    parse_bloc_line - read 8 lines of @sizex blocs of 8 and return a vector of @sizex Blocs (a structure with vectors of 64 aka a 8x8 bloc)
    impl do_dct for Bloc - do the dct over a bloc values
    impl displays for Bloc - function to display a bloc as a matrix in the terminal
    Also implemented error management, most function that can fail return Result with custom errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,122 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::f64::consts::PI;
+use std::fs;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Lines;
+
+
+#[derive(Debug)]
+enum JpegError {
+    FileError,
+    HelpError,
+    ReadError,
+}
+
+fn c_function(u: u8) -> f64 {
+    if u == 0 {
+        1.0/(2.0_f64).sqrt()
+    } else {
+        1.0
+    }
+}
+
+fn dct_cos(x: u8, i: u8) -> f64 {
+    f64::cos((2*(x/8)+1) as f64 *(i/8) as f64 *PI/16.0)
+    * f64::cos((2*(x%8)+1) as f64 *(i%8) as f64 *PI/16.0)
+}
+
+struct Bloc {
+    raw_data: [u8; 64],
+    dct_data: [i64; 64],
+}
+
+impl Bloc {
+    fn do_dct(&mut self) {
+        for i in 0..64 {
+            self.dct_data[i] = (self.raw_data
+                .into_iter()
+                .fold(0.0_f64, |acc, x| acc+(x as f64)*dct_cos(x,i as u8))
+                * 0.25 * c_function(i as u8/8) * c_function(i as u8%8)) as i64;
+        };
+    }
+
+    fn display_raw(&self) {
+        for i in 0..8 {
+            for u in 0..8 {
+                print!("{} ",self.raw_data[i*8+u])
+            }
+            println!();
+        }
+    }
+
+    fn display_dct(&self) {
+        for i in 0..8 {
+            for u in 0..8 {
+                print!("{} ",self.dct_data[i*8+u])
+            }
+            println!();
+        }
+    }
+}
+
+fn help(cmd: &String) -> JpegError {
+    println!("usage: {cmd} -f 'path_to_image'");
+    JpegError::HelpError
+}
+
+fn parse_cmd(args: Vec<String>) -> Result<Lines<BufReader<fs::File>>, JpegError> {
+    match args.len() {
+        3 => {
+            if args[1].eq("-f") {
+                let Ok(file) = fs::File::open(&args[2]) else {
+                    return Err(JpegError::FileError);
+                };
+                Ok(BufReader::new(file).lines())
+            } else {
+                Err(help(&args[0]))
+            }
+        },
+        _ => {
+            Err(help(&args[0]))
+        },
+    }
+}
+
+fn parse_bloc_line(mut lines: Lines<BufReader<fs::File>>, sizex: u32) -> Result<Vec<Bloc>, JpegError> {
+    let mut blocs: Vec<Bloc> = Vec::new();
+    for _ in  0..sizex { blocs.push(Bloc { raw_data: [0;64], dct_data: [0;64] }); };
+
+    for bline in 0..8 {
+        let Some(Ok(line)) = lines.next() else {
+            return Err(JpegError::ReadError);
+        };
+        let mut count = 0;
+        let mut errors = false;
+        line
+            .split(" ")
+            .into_iter()
+            .map(|s| s.parse::<u8>())
+            .filter_map(|r| r.map_err(|_|errors=true).ok())
+            .for_each(|x| {
+                blocs[count/8].raw_data[8*bline + count%8] = x;
+                count+=1;});
+        if errors {
+            return Err(JpegError::ReadError);
+        }
+    };
+
+    Ok(blocs)
+}
+
+fn main() -> Result<(), JpegError>{
+
+    let args: Vec<String> = env::args().collect();
+    let lines = parse_cmd(args)?;
+    let mut blocs = parse_bloc_line(lines, 2)?;
+    blocs.iter_mut().for_each(|b| b.do_dct());
+    blocs[0].display_raw();
+    blocs[0].display_dct();
+
+    Ok(())
 }
